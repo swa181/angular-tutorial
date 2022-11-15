4e8db5ee3829bfd6d60f9789b2eaa73f71e7bb0b
Sandy <swa181@sfu.ca>
2022-11-15 15:02:02 -0800

    section 10 update

---
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-home',
+  templateUrl: './collections-home.component.html',
+  styleUrls: ['./collections-home.component.css'],
+})
+export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {
+      name: 'James',
+      age: 24,
+      job: 'Designer',
+      employed: true,
+    },
+    {
+      name: 'Jill',
+      age: 26,
+      job: 'Engineer',
+    },
+    {
+      name: 'Elyse',
+      age: 24,
+      job: 'Engineer',
+    },
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
