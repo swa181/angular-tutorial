f49149f29a1f37ce483f992969d1bd211751bd0d
Sandy <swa181@sfu.ca>
2022-11-15 16:13:08 -0800

    sec11 semantic modules

--- comps/src/app/mods/modal/modal.component.ts
+++ comps/src/app/mods/modal/modal.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Output() close = new EventEmitter();
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick() {
+    this.close.emit();
+  }
 }
--- comps/src/app/mods/mods-home/mods-home.component.ts
+++ comps/src/app/mods/mods-home/mods-home.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
+  modalOpen = false;
+  items = [
+    { title: 'q1', content: 'a1' },
+    { title: 'q2', content: 'a2' },
+    { title: 'q3', content: 'a3' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick() {
+    this.modalOpen = !this.modalOpen;
   }
-
 }
